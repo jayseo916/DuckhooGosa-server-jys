f7b17630dcbcd5ecc839255b81e601a6905ccd03
Kang Taehoon <jadetypehoon@gmail.com>
2019-09-19 20:03:53 +0900

    DELETE

--- django-rest/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mac/WebstormProjects/4WEEKS/DuckhooGosa-server/django-rest/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
