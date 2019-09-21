dbdba40fef4e1dc2ec6c74e60d6d0a2680d93dfb
Kang Taehoon <jadetypehoon@gmail.com>
2019-09-21 11:08:49 +0900

    Setting api base

--- app/app.py
+++ app/app.py
@@ -27,11 +27,14 @@ connection = MongoClient(conf_host,
                          password=conf_password,
                          authSource="duck",
                          authMechanism='SCRAM-SHA-256')
-
 db = connection.duck
+
+# 테스트용 스키마
 tool = db.tool
 posts = db.posts
+# 실제 사용 스키마
 commentsCollections = db.comments
+problemsCollections = db.problems
 
 app = Flask(__name__)
 api = Api(app)
@@ -40,16 +43,17 @@ app.secret_key = getattr(sys.modules[__name__], 'FN_FLASK_SECRET_KEY')
 app.register_blueprint(google_auth.app)
 
 
+# 구글 연동용으로 카피해놓은 코드. 실제 프로젝트 무관
 @app.route('/')
 def index():
     if google_auth.is_logged_in():
         user_info = google_auth.get_user_info()
         return '<div>You are currently logged in as ' + user_info['given_name'] + '<div><pre>' + json.dumps(user_info,
                                                                                                             indent=4) + "</pre>"
-
     return 'You are not currently logged in.'
 
 
+# json 쪼개는 로직
 parser = reqparse.RequestParser()
 parser.add_argument('task')
 parser.add_argument('email')
@@ -120,13 +124,53 @@ class Comment(Resource):
         return json.dumps(obj), 201
 
 
+# 이번 구현목표
+class Problem(Resource):
+    def get(self, problem_id):
+        result = problemsCollections.find_all({"problem_id": problem_id})
+        return TODOS
+
+    def post(self):
+        return "good!"
+
+
+class ProblemMain(Resource):
+    def GET(self):
+        return "good!"
+
+
+class ProblemSearch(Resource):
+    def GET(self):
+        return "good!"
+
+
+class ProblemSolution(Resource):
+    def POST(self):
+        return "good!"
+
+
+class ProblemEvalation(Resource):
+    def POST(self):
+        return "good!"
+
+
 # URL Router에 맵핑한다.(Rest URL정의)
 api.add_resource(TodoList, '/todos/')
 api.add_resource(Todo, '/todos/<string:todo_id>')
 
-# api.add_resource(Comments, '/Comments/<string:Comments_id>')
+# comments _ POST
 api.add_resource(Comment, '/comments/')
+# comments _ GET
 api.add_resource(CommentList, '/comments/<string:problem_id>')
+
+# problem _ GET
+api.add_resource(ProblemMain, '/problems/main')
+api.add_resource(ProblemSearch, '/problems/search/<string:tag_word>')
+api.add_resource(Problem, '/problems/<string:problem_id>')
+# problem _ POST
+api.add_resource(ProblemSolution, '/problems/solution')
+api.add_resource(ProblemEvalation, '/problems/evalation')
+api.add_resource(Problem, '/problems/')
 
 # 서버 실행
 if __name__ == '__main__':
